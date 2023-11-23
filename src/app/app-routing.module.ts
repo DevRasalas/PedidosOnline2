eb6edfc71e91b8be4883278672e59e24653c805a
DevRasalas <agusportill21@gmail.com>
2023-11-23 00:30:40 -0300

    porquemeobliga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { AgregarProductosComponent } from './agregar-productos/agregar-productos.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
